59e822faec1d072c690913fcacde491e0b3d69b7
Artem Stepanenko <artem.stepanenko.ks.ua@gmail.com>
2019-12-03 22:29:17 +0200

    -delete calling exit() on engine; - move congratulations() to 'else' branch

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -10,14 +10,15 @@ def run(generate_round):
     while right_answers < settings.MIN_CORRECT_ANSWERS:
         question, true_answer = generate_round.task()
         user_answer = cli.get_answer(question)
-        if user_answer == true_answer:
-            cli.set_responce(CORRECT)
-            right_answers += 1
-        else:
+        if user_answer != true_answer:
             cli.set_responce(
                 INCORRECT.format(
                     user_answer,
                     true_answer,
                     user_name))
-            exit()
-    cli.congratulations(user_name)
+            break
+        else:
+            right_answers += 1
+            cli.set_responce(CORRECT)
+            if right_answers == 3:
+                cli.congratulations(user_name)
