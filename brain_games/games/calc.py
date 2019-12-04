7d5519b1f76af788b825637c0d692c8e78a6de54
Artem Stepanenko <artem.stepanenko.ks.ua@gmail.com>
2019-12-04 09:29:16 +0200

    <brain_calc.py> - add module "operator"

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -1,15 +1,17 @@
 from settings import MIN, MAX
 import random
+import operator
 
 
 TITLE = "What is the result of the expression?"
 
 
 def task():
+
     a = random.randint(MIN, MAX)
     b = random.randint(MIN, MAX)
-    variants = [('{} + {}'.format(a, b), a + b),
-                ('{} - {}'.format(a, b), a - b),
-                ('{} * {}'.format(a, b), a * b)]
+    variants = [('{} + {}'.format(a, b), operator.add(a, b)),
+                ('{} - {}'.format(a, b), operator.sub(a, b)),
+                ('{} * {}'.format(a, b), operator.mul(a, b))]
     question, true_answer = random.choice(variants)
     return question, str(true_answer)
